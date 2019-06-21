597e4b5672cd185a53ce0bf5a90bff645c337213
sam1elf <samuel_walter@gmx.de>
2019-06-21 16:23:17 +0200

    final commit
    styling changes

--- Javascript tictactoe/functions.js
+++ Javascript tictactoe/functions.js
@@ -2,14 +2,14 @@ var currentPlayer = "O";
 var winner = "";
 counter = 0;
 
-
 function tilePressed(id) {
     // console.log(id +" pressed!");
     if ( winner === "") {
         if ($('#' + id).text() === "--" ) {
             $('#' + id).text(currentPlayer);
             counter ++;
-
+            
+            // check if all 9 tiles have been played
             if ( counter == 9 ) {
                 $('#output').text("No Winner. Try again!");
                 $('#playAgain').attr('class', 'btn-lg btn-success mx-auto visible');
@@ -19,24 +19,24 @@ function tilePressed(id) {
             if ( isThereAWinner() ) {
                 $('#output').text(currentPlayer + " Wins!");
                 winner = currentPlayer;
+                
             };
-
-            // switch currentPlayer
-            if ( currentPlayer == "O" ) {
-                currentPlayer = "X";
-                $('#currPlay').text("X");
-            } else {
-                currentPlayer = "O";
-                $('#currPlay').text("O");
-            };
-
+            if ( winner === "" && counter < 9) {
+                // switch currentPlayer
+                if ( currentPlayer == "O" ) {
+                    currentPlayer = "X";
+                    $('#output').text("Current Player: X");
+                } else {
+                    currentPlayer = "O";
+                    $('#output').text("Current Player: O");
+                };
+            }
         } 
     }
 };
 
 
 function isThereAWinner() {
-    console.log("entering winner eval");
     if (
     $('#button1').text() === currentPlayer && $('#button2').text() === currentPlayer && $('#button3').text() === currentPlayer ||
     $('#button4').text() === currentPlayer && $('#button5').text() === currentPlayer && $('#button6').text() === currentPlayer ||
@@ -47,11 +47,9 @@ function isThereAWinner() {
     $('#button1').text() === currentPlayer && $('#button5').text() === currentPlayer && $('#button9').text() === currentPlayer ||
     $('#button7').text() === currentPlayer && $('#button5').text() === currentPlayer && $('#button3').text() === currentPlayer  
     ) {
-        console.log("returning true");
         $('#playAgain').attr('class', 'btn-lg btn-success mx-auto visible');
         return true;
     } else {
-        console.log("returning false");
         return false;
     };
             
